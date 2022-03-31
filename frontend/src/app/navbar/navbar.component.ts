e2134bef1d9cb551f44bae200930cd9c452b9ba2
Stef Korporaal <stefkorporaal@gmail.com>
2022-03-31 13:25:18 +0200

    Laatste versie feedback

--- frontend/src/app/navbar/navbar.component.ts
+++ frontend/src/app/navbar/navbar.component.ts
@@ -7,7 +7,7 @@ import { NavigationService, NavItem, UserType } from '../navigation.service';
   styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
-  debugUserType: UserType = 'guest';
+  debugUserType: UserType = 'user';
   private navItems: NavItem[] = [];
 
   constructor(private navigationService: NavigationService) {}
@@ -45,6 +45,10 @@ export class NavbarComponent implements OnInit {
       default:
         throw `Unknown user type ${currentUserType}`;
     }
+  }
+
+  showMenuButton(): boolean {
+    return Boolean(this.getNavigationItems().length);
   }
 
   /**
--- frontend/src/app/shortcuts/shortcuts.component.ts
+++ frontend/src/app/shortcuts/shortcuts.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+
+type Mode = 'desktop' | 'mobile';
 
 @Component({
   selector: 'app-shortcuts',
@@ -6,10 +8,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./shortcuts.component.scss'],
 })
 export class ShortcutsComponent {
-  constructor() {}
+  private mode: Mode;
+  @ViewChild('chipList') chipList!: ElementRef;
+
+  constructor() {
+    this.mode = this.isMobile() ? 'mobile' : 'desktop';
+  }
 
   isMobile(): boolean {
     const query = window.matchMedia('(max-width: 640px)');
     return query.matches;
+  }
+
+  onResize(_: Event) {
+    if (this.isMobile() && this.mode == 'desktop') {
+      this.chipList.nativeElement.classList.add('mat-chip-list-stacked');
+      this.mode = 'mobile';
+    }
+
+    if (!this.isMobile() && this.mode == 'mobile') {
+      this.chipList.nativeElement.classList.remove('mat-chip-list-stacked');
+      this.mode = 'desktop';
+    }
   }
 }
