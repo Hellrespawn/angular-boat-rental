e72ad5eccceafb0c5f58e39886861dd2bc4000aa
Stef Korporaal <stefkorporaal@gmail.com>
2022-04-01 14:38:15 +0200

    Gebruik BreakpointObserver om isMobile te checken

--- frontend/src/app/home/shortcuts/shortcuts.component.ts
+++ frontend/src/app/home/shortcuts/shortcuts.component.ts
@@ -1,6 +1,6 @@
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Component, ElementRef, ViewChild } from '@angular/core';
-
-type Mode = 'desktop' | 'mobile';
+import { map, Observable, shareReplay } from 'rxjs';
 
 @Component({
   selector: 'app-shortcuts',
@@ -8,27 +8,11 @@ type Mode = 'desktop' | 'mobile';
   styleUrls: ['./shortcuts.component.scss'],
 })
 export class ShortcutsComponent {
-  private mode: Mode;
   @ViewChild('chipList') chipList!: ElementRef;
 
-  constructor() {
-    this.mode = this.isMobile() ? 'mobile' : 'desktop';
-  }
+  constructor(private breakpointObserver: BreakpointObserver) {}
 
-  isMobile(): boolean {
-    const query = window.matchMedia('(max-width: 640px)');
-    return query.matches;
-  }
-
-  onResize(_: Event) {
-    if (this.isMobile() && this.mode == 'desktop') {
-      this.chipList.nativeElement.classList.add('mat-chip-list-stacked');
-      this.mode = 'mobile';
-    }
-
-    if (!this.isMobile() && this.mode == 'mobile') {
-      this.chipList.nativeElement.classList.remove('mat-chip-list-stacked');
-      this.mode = 'desktop';
-    }
-  }
+  isMobile$: Observable<boolean> = this.breakpointObserver
+    .observe(['(max-width: 640px)'])
+    .pipe(map((state) => state.matches));
 }
