b06eb36e4bd69fdc895a9c48bb6d0b0f2d47b396
Kees <kees.van.ruler@hva.nl>
2022-04-11 09:33:46 +0200

    skipper overview component aangemaakt

---
+++ frontend/src/app/admin-skipper-overview/admin-skipper-overview.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AdminSkipperOverviewComponent } from './admin-skipper-overview.component';
+
+describe('AdminSkipperOverviewComponent', () => {
+  let component: AdminSkipperOverviewComponent;
+  let fixture: ComponentFixture<AdminSkipperOverviewComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AdminSkipperOverviewComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AdminSkipperOverviewComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ frontend/src/app/admin-skipper-overview/admin-skipper-overview.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-skipper-overview',
+  templateUrl: './admin-skipper-overview.component.html',
+  styleUrls: ['./admin-skipper-overview.component.scss']
+})
+export class AdminSkipperOverviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
