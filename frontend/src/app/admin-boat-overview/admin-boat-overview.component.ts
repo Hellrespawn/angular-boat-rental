d93281f5573384812a47aa5645d397d51a600330
Kees <kees.van.ruler@hva.nl>
2022-04-14 13:53:07 +0200

    snackbar toegevoegd aan beheerspanels van skippers en boten

--- frontend/src/app/admin-boat-overview/admin-boat-overview.component.ts
+++ frontend/src/app/admin-boat-overview/admin-boat-overview.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { BoatService } from '../boat-service.service';
 import { addToNavBar } from '../navigation.service';
+import { SnackBarInput, SnackBarService } from '../snack-bar.service';
 
 @addToNavBar({
   name: 'Boot-administratie',
@@ -14,7 +15,16 @@ import { addToNavBar } from '../navigation.service';
 })
 export class AdminBoatOverviewComponent implements OnInit {
   public arrayOfBoats: Array<BoatForAdmin> = [];
-  constructor(private boatService: BoatService) {}
+  private readonly succesSnackbarInput: SnackBarInput = {
+    message: 'Boot is verwijderd!',
+    buttonText: 'Sluit',
+    duration: 2000,
+    error: false,
+  };
+  constructor(
+    private boatService: BoatService,
+    private snackBarService: SnackBarService
+  ) {}
 
   ngOnInit(): void {
     this.getBoatsFromDatabase();
@@ -27,6 +37,9 @@ export class AdminBoatOverviewComponent implements OnInit {
   public async deleteBoatById(id: number, index: number): Promise<void> {
     this.boatService.deleteBoatById(id).subscribe(() => {
       this.arrayOfBoats.splice(index, 1);
+      this.snackBarService.makeSnackbarThatClosesAutomatically(
+        this.succesSnackbarInput
+      );
     });
   }
   public async updateMaintenance(
--- frontend/src/app/admin-skipper-overview/admin-skipper-overview.component.ts
+++ frontend/src/app/admin-skipper-overview/admin-skipper-overview.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { addToNavBar } from '../navigation.service';
 import { SkipperService } from '../skipper.service';
+import { SnackBarInput, SnackBarService } from '../snack-bar.service';
 
 @addToNavBar({
   name: 'Skipper-administratie',
@@ -14,7 +15,16 @@ import { SkipperService } from '../skipper.service';
 })
 export class AdminSkipperOverviewComponent implements OnInit {
   public arrayOfSkippers: Array<SkipperForAdmin> = [];
-  constructor(private skipperService: SkipperService) {}
+  private readonly succesSnackbarInput: SnackBarInput = {
+    message: 'Schipper is verwijderd!',
+    buttonText: 'Sluit',
+    duration: 2000,
+    error: false,
+  };
+  constructor(
+    private skipperService: SkipperService,
+    private snackBarService: SnackBarService
+  ) {}
 
   ngOnInit(): void {
     this.getSkippersFromDatabase();
@@ -27,6 +37,9 @@ export class AdminSkipperOverviewComponent implements OnInit {
   public async deleteBoatById(id: number, index: number): Promise<void> {
     this.skipperService.deleteSkipperById(id).subscribe(() => {
       this.arrayOfSkippers.splice(index, 1);
+      this.snackBarService.makeSnackbarThatClosesAutomatically(
+        this.succesSnackbarInput
+      );
     });
   }
   public parseDateStringToDate(dateString: string | Date): Date {
