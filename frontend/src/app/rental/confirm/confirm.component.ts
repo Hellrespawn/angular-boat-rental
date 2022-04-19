505fc87589f06bea385a6bc66c8a1af08072d11c
Stef Korporaal <stefkorporaal@gmail.com>
2022-04-19 11:48:17 +0200

    Linting error

--- frontend/src/app/rental/confirm/confirm.component.ts
+++ frontend/src/app/rental/confirm/confirm.component.ts
@@ -8,5 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ConfirmComponent implements OnInit {
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    console.log('Unimplemented');
+  }
 }
