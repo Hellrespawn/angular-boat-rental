54c3e56dfd079c1197b7b8707f487311d0cce572
Kees <kees.van.ruler@hva.nl>
2022-04-21 12:12:11 +0200

    generated new customer overview component for the admin

---
+++ frontend/src/app/admin-customer-overview/admin-customer-overview.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-customer-overview',
+  templateUrl: './admin-customer-overview.component.html',
+  styleUrls: ['./admin-customer-overview.component.scss']
+})
+export class AdminCustomerOverviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
