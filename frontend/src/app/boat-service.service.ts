0f50e554620db1e15d22edb2463a65aaf4de2f5f
Kees <kees.van.ruler@hva.nl>
2022-04-26 11:14:18 +0200

    added code comments to the boat service (frontend)

--- frontend/src/app/boat-service.service.ts
+++ frontend/src/app/boat-service.service.ts
@@ -1,10 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, map } from 'rxjs';
-import { BoatOverviewData } from './rental/rental.component';
-import { Boat, BoatRequirements } from './boat';
+import { Observable } from 'rxjs';
+import { Boat } from './boat';
 import { environment } from 'src/environments/environment';
-import { BoatDetailData } from './rental/boat-card/boat-details/boat-details.component';
 
 @Injectable({
   providedIn: 'root',
@@ -12,19 +10,39 @@ import { BoatDetailData } from './rental/boat-card/boat-details/boat-details.com
 export class BoatService {
   constructor(private httpClient: HttpClient) {}
 
-  public addBoat(boatObject: {}) {
+  /**
+   * sends a request to the backend to add a new boat to the database
+   * @param boatObject the object specifying the new boat
+   * @returns and Observable of either the newly added boat or an error object
+   */
+  public addBoat(boatObject: {}): Observable<any> {
     return this.httpClient.post(`${environment.backendUrl}/boats`, boatObject);
   }
 
+  /**
+   * sends a request to the backend to fetch all Boats from the database
+   * @returns an Observable of an array of all Boats in the database
+   */
   public getBoats(): Observable<any> {
     return this.httpClient.get<{ boats: Boat[] }>(
       `${environment.backendUrl}/boats`
     );
   }
 
+  /**
+   * sends a request to the backend to delete a specific boat
+   * @param id the id of the boat that needs to be deleted
+   * @returns an Observable of the response object
+   */
   public deleteBoatById(id: number): Observable<Object> {
     return this.httpClient.delete(`${environment.backendUrl}/boats/${id}`);
   }
+  /**
+   * sends a request to the backend to update the maintenance boolean value of a specific boat
+   * @param id the id of the specific boat
+   * @param updatedValue the new value of the maintenance boolean of the specific boat
+   * @returns an Observable of the response object
+   */
   public updateMaintenanceStatus(
     id: number,
     updatedValue: boolean
