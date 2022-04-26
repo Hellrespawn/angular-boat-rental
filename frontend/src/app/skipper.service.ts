0dd400f6e134f5cf835849185225727f7d9cd491
Kees <kees.van.ruler@hva.nl>
2022-04-26 11:03:39 +0200

    refactored skipper service (frontend) and added code comments

--- frontend/src/app/skipper.service.ts
+++ frontend/src/app/skipper.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, map } from 'rxjs';
+import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Skipper } from './skipper';
 
@@ -10,21 +10,41 @@ import { Skipper } from './skipper';
 export class SkipperService {
   constructor(private httpClient: HttpClient) {}
 
-  public addSkipper(skipperObject: {}) {
+  /**
+   * sends a new skipper object to the backend, in which it will be stored in the database
+   * @param skipperObject object describing the new skipper
+   * @returns an observable of either the created skipper or an error object
+   */
+  public addSkipper(skipperObject: {}): Observable<Object> {
     return this.httpClient.post(
       `${environment.backendUrl}/skippers`,
       skipperObject
     );
   }
 
+  /**
+   * requests all skippers from the backend
+   * @returns an observable of an array of skippers (can be empty)
+   */
   public getSkippers(): Observable<any> {
     return this.httpClient.get<{ skippers: Skipper[] }>(
       `${environment.backendUrl}/skippers`
     );
   }
+  /**
+   * sends a request for deletion of a specific skipper to the backend
+   * @param id id of the skipper which needs to be deleted
+   * @returns an Observable of the return object of the deletion
+   */
   public deleteSkipperById(id: number): Observable<Object> {
     return this.httpClient.delete(`${environment.backendUrl}/skippers/${id}`);
   }
+  /**
+   * updates the leave boolean to the updated value of a specific skipper found by id
+   * @param id id of specific skipper
+   * @param updatedValue updated value of the leave boolean
+   * @returns an Observable of the response object
+   */
   public updateLeaveStatus(
     id: number,
     updatedValue: boolean
