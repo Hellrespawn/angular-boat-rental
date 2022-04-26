eec0f832a215767b02d4fdd0f3d71159a8b4c8ac
Kees <kees.van.ruler@hva.nl>
2022-04-26 11:07:53 +0200

    added code comments to the customer service (frontend)

--- frontend/src/app/customer.service.ts
+++ frontend/src/app/customer.service.ts
@@ -10,14 +10,29 @@ import { Customer } from './customer';
 export class CustomerService {
   constructor(private httpClient: HttpClient) {}
 
+  /**
+   * sends a request to the backend for all skippers in the database
+   * @returns an Observable of an array of Customers
+   */
   public getCustomers(): Observable<any> {
     return this.httpClient.get<{ customers: Customer[] }>(
       `${environment.backendUrl}/customers`
     );
   }
+  /**
+   * sends a request to the backend to delete a specific Customer by id
+   * @param id id of the specific Cutsomer
+   * @returns an Observable of the response object
+   */
   public deleteCustomerById(id: number): Observable<Object> {
     return this.httpClient.delete(`${environment.backendUrl}/customers/${id}`);
   }
+  /**
+   * sends a request to the backend to update the blocked boolean of a specific Customer by id
+   * @param id the id of the customer needed to be updated
+   * @param updatedValue the new value of the blocked boolean of specific customer
+   * @returns an Observable of the response object
+   */
   public updateBlockedStatus(
     id: number,
     updatedValue: boolean
