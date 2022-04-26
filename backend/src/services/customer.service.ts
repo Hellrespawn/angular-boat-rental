2a2b3e57928a36b4ef151b56b12b1c1a4d5638c9
Kees <kees.van.ruler@hva.nl>
2022-04-26 10:41:31 +0200

    customer service added codecomments and refactored

--- backend/src/services/customer.service.ts
+++ backend/src/services/customer.service.ts
@@ -1,16 +1,17 @@
 import { Customer } from '../model/customer.model';
 export class CustomerService {
-  private customerArray: Customer[] = [];
-
-  private async updateCustomers(): Promise<void> {
-    this.customerArray = await Customer.findAll();
-  }
-
+  /**
+   * returns all Customers from the database
+   */
   public async returnAllCustomers(): Promise<Array<Customer>> {
-    await this.updateCustomers();
-    return this.customerArray;
+    return await Customer.findAll();
   }
 
+  /**
+   * updates the blocked boolean of a specific Customer found by id
+   * @param idOfCustomer id of customer to be updated
+   * @param updatedValue new value of the blocked boolean
+   */
   public async updateCustomer(
     idOfCustomer: number,
     updatedValue: boolean
@@ -26,6 +27,10 @@ export class CustomerService {
     }
   }
 
+  /**
+   * delete a Customer found by id
+   * @param idOfCustomer the id of the customer to be deleted
+   */
   public async deleteCustomer(idOfCustomer: number): Promise<void> {
     const customerToDelete: Customer | null = await Customer.findByPk(
       idOfCustomer
