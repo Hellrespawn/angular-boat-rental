cffaf53c7b92ed4a388eb9d95bd878a0ee9440d4
Hans <hans.den.otter@hva.nl>
2022-04-28 13:56:29 +0200

    message to DB check

--- backend/tests/faq/send-msg.test.ts
+++ backend/tests/faq/send-msg.test.ts
@@ -18,16 +18,9 @@ describe('Test Message', () => {
   });
   after(closeDatabase);
 
-  it('Check DB connection', async () => {
-    const res = await request(app).get(`/faq`);
-    expect(res.status).to.equal(200);
-  });
-
   it('Check if fields are same as DB fields', async () => {
     const res = await request(app).get(`/faq`);
     console.log(res.body.message[0]);
-    // console.log(message.name)
-    // expect(res.status).to.equal(200);
     expect(res.body.message[0].name).to.equal('Testgebruiker1');
     expect(res.body.message[0].email).to.equal('Testgebruiker@hotmail.com');
     expect(res.body.message[0].text).to.equal('Lorem Ipsum');
