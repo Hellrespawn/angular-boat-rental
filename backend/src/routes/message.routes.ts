8f4ce83f632430c0dd9c1894df6dfaa3a30ba830
Hans <hans.den.otter@hva.nl>
2022-05-13 10:47:31 +0200

    code comments toegevoegd

--- backend/src/routes/message.routes.ts
+++ backend/src/routes/message.routes.ts
@@ -5,10 +5,12 @@ export function addMessageRoute(
   app: Application,
   controller: MessageController
 ): void {
+  // get request from backend url
   app.get('/faq', (req: Request, res: Response): void => {
     controller.getMessages(req, res);
   });
 
+  // post request to frontend url
   app.post(
     '/veel-gestelde-vragen',
     async (req: Request, res: Response): Promise<void> => {
--- frontend/src/app/faq/faq.component.ts
+++ frontend/src/app/faq/faq.component.ts
@@ -29,15 +29,8 @@ export class FaqComponent {
   @ViewChild('emailForm') public emailInp!: ElementRef<HTMLInputElement>;
   @ViewChild('textForm') public textInp!: ElementRef<HTMLInputElement>;
 
-  // input Formcontrol
-  public emailFormControl = new FormControl('', [
-    Validators.required,
-    Validators.email,
-  ]);
-  public nameFormControl = new FormControl('', [Validators.required]);
-  public textFormControl = new FormControl('', [Validators.required]);
-
-  // 2 functions to send the messages to the backend with user input
+  /* 2 functions to send the messages to the backend with user input
+   addMessages(this.getMessages()) invokes the message service with getMessages() function input */
   public sendMessageToBackend(): void {
     this.messageService.addMessages(this.getMessages()).subscribe();
     this.snackBService.makeSnackbarThatClosesAutomatically(
@@ -50,12 +43,17 @@ export class FaqComponent {
     let nameInp: string = this.nameInp.nativeElement.value;
     let emailInp: string = this.emailInp.nativeElement.value;
     let textInp: string = this.textInp.nativeElement.value;
-    console.log(nameInp);
-    console.log(emailInp);
-    console.log(textInp);
 
     return { name: nameInp, email: emailInp, text: textInp };
   }
+
+  // input Formcontrol
+  public emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
+  public nameFormControl = new FormControl('', [Validators.required]);
+  public textFormControl = new FormControl('', [Validators.required]);
 
   //  code for error message when user gives wrong input
   public nameErrorMessage(): string {
