8474b26e07a8609e6256b423c4fbb5a45e667bf7
Stef Korporaal <stefkorporaal@gmail.com>
2022-05-30 11:21:30 +0200

    Login tests

--- frontend/src/app/login/login.component.spec.ts
+++ frontend/src/app/login/login.component.spec.ts
@@ -1,4 +1,12 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { By } from '@angular/platform-browser';
+import { RouterTestingModule } from '@angular/router/testing';
+import { SessionService } from '../session.service';
+import { MockSessionService } from '../test/session.service.mock';
 
 import { LoginComponent } from './login.component';
 
@@ -6,19 +14,111 @@ describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
 
+  let button: DebugElement;
+  let emailInput: DebugElement;
+  let passwordInput: DebugElement;
+
+  let buttonEl: HTMLButtonElement;
+  let emailInputEl: HTMLInputElement;
+  let passwordInputEl: HTMLInputElement;
+
+  const validEmail = 'test0@test.test';
+  const validPassword = 'password';
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [LoginComponent],
-    }).compileComponents();
+      imports: [
+        HttpClientTestingModule,
+        MatSnackBarModule,
+        RouterTestingModule,
+        ReactiveFormsModule,
+      ],
+    })
+      .overrideComponent(LoginComponent, {
+        set: {
+          providers: [
+            { provide: SessionService, useClass: MockSessionService },
+          ],
+        },
+      })
+      .compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    button = fixture.debugElement.query(By.css('#LogIn-btn'));
+    buttonEl = button.nativeElement;
+
+    emailInput = fixture.debugElement.query(By.css('#emailadres-input'));
+    emailInputEl = emailInput.nativeElement;
+
+    passwordInput = fixture.debugElement.query(By.css('#wachtwoord-input'));
+    passwordInputEl = passwordInput.nativeElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('<input> should pass value to FormControl', async () => {
+    const email = 'stefkorporaal@gmail.com';
+    const password = 'password';
+
+    emailInputEl.value = email;
+    emailInputEl.dispatchEvent(new Event('input'));
+
+    passwordInputEl.value = password;
+    passwordInputEl.dispatchEvent(new Event('input'));
+
+    fixture.detectChanges();
+    await fixture.whenStable();
+
+    expect(component.email.value).toBe(email);
+    expect(component.wachtwoord.value).toBe(password);
+  });
+
+  it('should reject empty input.', async () => {
+    buttonEl.click();
+
+    fixture.detectChanges();
+    await fixture.whenStable();
+
+    expect(component.email.invalid).toBeTrue();
+    expect(component.wachtwoord.invalid).toBeTrue();
+  });
+
+  it('should reject invalid email address.', async () => {
+    emailInputEl.value = 'not an email';
+    emailInputEl.dispatchEvent(new Event('input'));
+
+    fixture.detectChanges();
+    await fixture.whenStable();
+
+    buttonEl.click();
+
+    fixture.detectChanges();
+    await fixture.whenStable();
+
+    expect(component.email.invalid).toBeTrue();
+  });
+
+  it('should send a request with valid values', async () => {
+    emailInputEl.value = validEmail;
+    emailInputEl.dispatchEvent(new Event('input'));
+
+    passwordInputEl.value = validPassword;
+    passwordInputEl.dispatchEvent(new Event('input'));
+
+    fixture.detectChanges();
+    await fixture.whenStable();
+
+    buttonEl.click();
+
+    fixture.detectChanges();
+    await fixture.whenStable();
   });
 });
