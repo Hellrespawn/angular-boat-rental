9bf7fce6b216f52d6aa207d5eff49f857b5ece02
Kees <kees.van.ruler@hva.nl>
2022-06-09 10:43:48 +0200

    finished all frontend tests for the add boat and boat overview pages

--- frontend/src/app/admin-skipper-overview/admin-skipper-overview.component.spec.ts
+++ frontend/src/app/admin-skipper-overview/admin-skipper-overview.component.spec.ts
@@ -1,11 +1,10 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { By } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
-import { Skipper } from '../skipper';
 import { SkipperService } from '../skipper.service';
 import { MockSkipperService } from '../test/skipper.service.mock';
 
@@ -30,6 +29,7 @@ describe('AdminSkipperOverviewComponent', () => {
         MatSnackBarModule,
         RouterTestingModule,
         ReactiveFormsModule,
+        BrowserAnimationsModule,
       ],
     })
       .overrideComponent(AdminSkipperOverviewComponent, {
