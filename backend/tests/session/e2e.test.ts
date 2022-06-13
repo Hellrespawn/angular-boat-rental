83c9f99f52a84d07a09474033d9a863bccc3351b
Stef Korporaal <stefkorporaal@gmail.com>
2022-06-13 14:26:15 +0200

    Expand login e2e test

--- backend/tests/session/e2e.test.ts
+++ backend/tests/session/e2e.test.ts
@@ -8,6 +8,20 @@ import { app } from '../../src/server';
 
 const SANDBOX = sinon.createSandbox();
 
+function getSessionIdFromCookie(cookies: [string]): string {
+  const cookie = cookies.join(';');
+  const pair = cookie
+    .split(';')
+    .map((s) => s.split('='))
+    .find(([key, _]) => key === 'session');
+
+  if (!pair) {
+    throw new Error('Cookie not set!');
+  }
+
+  return pair[1];
+}
+
 describe('Test Session & Login end-to-end', () => {
   const email = 'test@test.test';
   const password = 'password';
@@ -23,12 +37,26 @@ describe('Test Session & Login end-to-end', () => {
     SANDBOX.restore();
   });
 
-  it('Allows access to `/login` without authentication.', async () => {
-    const res = await request(app)
+  it('Allows access to `/login` with and without authentication.', async () => {
+    const agent = request(app);
+
+    // Without auth
+    const res1 = await agent
       .post('/login')
       .send({ email: user.emailAddress, password });
 
-    expect(res.status).to.equal(200);
+    expect(res1.status).to.equal(200);
+    const cookie1 = getSessionIdFromCookie(res1.headers['set-cookie']);
+
+    // With auth
+    const res2 = await agent
+      .post('/login')
+      .send({ email: user.emailAddress, password });
+
+    expect(res2.status).to.equal(200);
+    const cookie2 = getSessionIdFromCookie(res2.headers['set-cookie']);
+
+    expect(cookie1).to.not.equal(cookie2);
   });
 
   it('Correctly sets sessionData', async () => {
