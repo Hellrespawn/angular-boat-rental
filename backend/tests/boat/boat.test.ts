e45e51d4b57aacf7742153d3ec2dc04e9db79227
Kees <kees.van.ruler@hva.nl>
2022-06-14 14:19:59 +0200

    wrote some backend tests

--- backend/tests/boat/boat.test.ts
+++ backend/tests/boat/boat.test.ts
@@ -10,6 +10,8 @@ import { SessionDao } from '../../src/database/session.dao';
 import { Session } from '../../src/model/session';
 import { User } from '../../src/model/user';
 import { SessionService } from '../../src/services/session.service';
+import { assert } from 'console';
+import { ServerError } from '../../src/util/error';
 
 describe('Test Boat-functionality in backend', () => {
   let boatService: BoatService;
@@ -161,5 +163,43 @@ describe('Test Boat-functionality in backend', () => {
   it('The deleteBoat method of the BoatDao should be called when correctly requested by the BoatService', () => {
     boatService.deleteBoat(1);
     expect(boatDaoDeletionSpy.callCount).to.equal(1);
+  });
+
+  //unit tests:
+
+  it('should throw an error when trying to make a Boat when a negative registrationNumber is entered', () => {
+    expect(() =>
+      Boat.createBoat('test', -1, 250, true, '', 25, 10, 'motor', 100)
+    ).to.throw('invalid registration number');
+  });
+
+  it('should throw an error when trying to make a Boat when a registrationNumber of zero is entered', () => {
+    expect(() =>
+      Boat.createBoat('test', 0, 250, true, '', 25, 10, 'motor', 100)
+    ).to.throw('invalid registration number');
+  });
+
+  it('should throw an error when trying to make a Boat when a negative price is entered', () => {
+    expect(() =>
+      Boat.createBoat('test', 123, -1, true, '', 25, 10, 'motor', 100)
+    ).to.throw('invalid registration number');
+  });
+
+  it('should throw an error when trying to make a Boat when a price of zero is entered', () => {
+    expect(() =>
+      Boat.createBoat('test', 123, 0, true, '', 25, 10, 'motor', 100)
+    ).to.throw('invalid registration number');
+  });
+
+  it('should throw an error when trying to make a Boat when a negative length is entered', () => {
+    expect(() =>
+      Boat.createBoat('test', 123, 250, true, '', -1, 10, 'motor', 100)
+    ).to.throw('invalid registration number');
+  });
+
+  it('should throw an error when trying to make a Boat when a length of zero is entered', () => {
+    expect(() =>
+      Boat.createBoat('test', 123, 250, true, '', 0, 10, 'motor', 100)
+    ).to.throw('invalid registration number');
   });
 });
